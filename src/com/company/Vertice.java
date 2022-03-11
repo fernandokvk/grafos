ff0caa40900ea853a1e643843611235f07e0d17f
Fernando <tukori2@gmail.com>
2022-03-11 07:59:57 -0300

    .

---
+++ src/com/company/Vertice.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class Vertice {
+    int num;
+    List<Vertice> adj;
+    int grauSaida;
+
+    public Vertice(int num) {
+        this.num = num;
+        this.adj = new LinkedList<Vertice>();
+    }
+
+    public String toString() {
+        return "Vertice(" + num + ")";
+    }
+}
